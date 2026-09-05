224dd08092a23f82bd2d3e42ccade59c9275b7c5
Sidrasi12 <guilhermerasi@gmail.com>
2026-09-05 16:37:21 -0300

    Update Firebase configuration for new project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,12 +5,12 @@ window.BOLAO_CONFIG = {
   entryFee: 100,
   adminEmails: ['guilhermerasi@gmail.com'],
   firebase: {
-    apiKey: "AIzaSyAjH8pM4px1-YG91wTWD74AeIfJd_VmioQ",
-  authDomain: "bolao-nfl-2026.firebaseapp.com",
-  projectId: "bolao-nfl-2026",
-  storageBucket: "bolao-nfl-2026.firebasestorage.app",
-  messagingSenderId: "16639131042",
-  appId: "1:16639131042:web:786ced81047b233e96c6fb"
+      apiKey: "AIzaSyCKyyqYifiWLklVn79mKxMsMDNEWYYTdHc",
+  authDomain: "bolao-nfl-tdc-2026.firebaseapp.com",
+  projectId: "bolao-nfl-tdc-2026",
+  storageBucket: "bolao-nfl-tdc-2026.firebasestorage.app",
+  messagingSenderId: "864243167678",
+  appId: "1:864243167678:web:6ea12d6be6c940c95b89ef"
   }
 };
 
